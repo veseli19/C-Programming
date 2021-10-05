3a870ba7d28702b4adfebd638d5f8de7dfd78097
Veselin Rafailovic <veselin.rafailovic@gmail.com>
2021-10-05 23:14:57 +0200

    new line

--- Test.c
+++ Test.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>                                  //enthält Informationen about standard input/output
 
-int main(void){
-    printf("Hello, C World\n");
+int main(void){                                        
+    printf("Hello, C World\n");                     //main calls a library function "printf" and print output. "\n" stays for new line, without it there is no new line
+    printf("Hi there!\n");
 
     return 0;
 }
