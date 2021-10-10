6c16543f4f50e6dae7be68d65691fb0f0bf21624
Veselin Rafailovic <veselin.rafailovic@gmail.com>
2021-10-10 13:32:41 +0200

    adjusting pointer

--- goto.c
+++ goto.c
@@ -5,6 +5,7 @@
 //function declaration - prototype
 double average(int, int, double);
 int point(int);
+void pointer();
 
 /*############################################################################################################*/
 //main mathod
@@ -12,6 +13,8 @@ int main(void){
 
     printf("Average = %.2f\n", average(100, 1, 0.0));
     printf("Value of variable p is %d\n", point(50));
+    printf("\n");
+    pointer();
     return 0;
 }
 /*##############################################################################################################*/
@@ -46,4 +49,16 @@ int point(int n){
     printf("Address of p variable is %x\n", p); //Print address from p
     return *p;
 
+}
+void pointer(){
+
+    int ar[] = {1, 2, 3, 4 ,5};
+    int *p;
+
+    p = &ar[0];   //storing address from first element in p
+    printf("%d\n", *p);  //1
+    *p += 10;            //first element will be 11
+    printf("%d\n", ar[0]); //11
+    p += 3;              //pointing on 4. element
+    printf("%d\n", *p);    //4
 }
